6a8297cb543381eec099cb58fd8b3010708b48c4
Irek <yairek@gmail.com>
2024-06-03 10:47:19 +0200

    Initial Commit

---
+++ TestOnlineShop/TestOnlineShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestOnlineShopApp.swift
+//  TestOnlineShop
+//
+//  Created by Reek i on 03.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestOnlineShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
